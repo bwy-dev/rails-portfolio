54089148e05b6a89b188d5f2ad4e10778f1df721
bwy-webd <benwordsworthyates@gmail.com>
2018-09-07 13:44:20 +0100

    implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,10 @@
 class Portfolio < ApplicationRecord
 
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	scope :angular, ->{where(subtitle: "Angular")}
+
+	scope :ruby_on_rails_portfolio_items, ->{where(subtitle: "Ruby on Rails")}
+
+	scope :game_development, ->{where(subtitle: "Game Development")}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+  Topic.create!(
+    title:"Topic #{topic}"
+    )
+end
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices lorem placerat erat aliquam, ac pretium sapien ultrices. In ullamcorper leo vel dui cursus iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse blandit arcu non maximus convallis. Nulla gravida lacinia nibh a ultrices. Nunc porta ante vitae eleifend volutpat. Suspendisse a leo vel risus suscipit aliquam. Sed venenatis sed libero a mollis."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices lorem placerat erat aliquam, ac pretium sapien ultrices. In ullamcorper leo vel dui cursus iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse blandit arcu non maximus convallis. Nulla gravida lacinia nibh a ultrices. Nunc porta ante vitae eleifend volutpat. Suspendisse a leo vel risus suscipit aliquam. Sed venenatis sed libero a mollis.",
+    topic_id: Topic.last.id
   )
 end
 puts "10 blog posts created"
@@ -16,13 +23,22 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: "My wonderful subtitle",
+    subtitle: "Ruby on Rails",
     body: "Pellentesque sagittis nisi in congue laoreet. Suspendisse vestibulum sapien id augue mattis congue ac rutrum lectus. Nunc mattis ipsum vel mauris viverra, quis condimentum dolor euismod. Sed condimentum purus sit amet urna finibus, in euismod nunc congue. Nulla at ornare diam.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200",
   )
 end
-puts "9 portfolio items created"  
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Pellentesque sagittis nisi in congue laoreet. Suspendisse vestibulum sapien id augue mattis congue ac rutrum lectus. Nunc mattis ipsum vel mauris viverra, quis condimentum dolor euismod. Sed condimentum purus sit amet urna finibus, in euismod nunc congue. Nulla at ornare diam.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200",
+  )
+end

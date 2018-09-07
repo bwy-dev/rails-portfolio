5b7457b53fc255fda3a7512a5be91785ffb840cc
bwy-webd <benwordsworthyates@gmail.com>
2018-09-07 11:01:33 +0100

    updated gemfile

--- db/migrate/20180627093757_add_default_value_to_status_attribute.rb
+++ db/migrate/20180627093757_add_default_value_to_status_attribute.rb
@@ -1,8 +1,5 @@
 class AddDefaultValueToStatusAttribute < ActiveRecord::Migration[5.2]
-  def up
+  def change
     change_column :blogs, :status, :integer, default: 0
-  end
-  def down
-    change_column :blogs, :status, :integer, default: nil
   end
 end
